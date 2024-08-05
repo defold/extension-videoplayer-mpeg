bde082852dc0b6ae6a4bb9600fe205fc95cc9942
Björn Ritzl <bjorn.ritzl@gmail.com>
2024-08-05 14:57:52 +0200

    Multiple instances, and basic streaming
    Fixes #2

--- README.md
+++ README.md
@@ -7,23 +7,27 @@ Native extension to play back MPEG1 video using [pl_mpeg](https://github.com/pho
 local buffer = resource.load(filename)
 
 -- open video
-assert(mpeg.open(buffer, { loop = true }))
+local handle, err = mpeg.open(buffer, { loop = true })
+if err then
+	error(err)
+	return
+end
 
 -- get video information
-local info = mpeg.get_info()
+local info = mpeg.get_info(handle)
 print(info.width, info.height)	-- video width and height
 print(info.time, info.duration)	-- current playback time and video duration
 
 -- get the buffer where video frames will be decoded
-local framebuffer = mpeg.get_frame()
+local framebuffer = mpeg.get_frame(handle)
 
 -- advance playback and decode a new frame
-mpeg.decode(seconds)
+mpeg.decode(handle, seconds)
 
 -- seek to exactly 10 seconds and decode a new frame
 local exact = true
-mpeg.seek(10, exact)
+mpeg.seek(handle, 10, exact)
 
 -- close video and release all resources
-mpeg.close()
+mpeg.close(handle)
 ```
--- mpeg/src/mpeg.cpp
+++ mpeg/src/mpeg.cpp
@@ -9,15 +9,17 @@
 #define PL_MPEG_IMPLEMENTATION
 #include "pl_mpeg.h"
 
-static const int s_BytesPerPixel = 3;
+static const int      BYTES_PER_PIXEL = 3;
+static const uint64_t FRAME_BUFFER_STREAM_NAME = dmHashString64("rgb");
+// static const size_t   STREAMING_BUFFER_CAPACITY = PLM_BUFFER_DEFAULT_SIZE;
+static const size_t   STREAMING_BUFFER_CAPACITY = 128 * 1024;
 
-
-static plm_t *g_plm;
-static dmBuffer::HBuffer g_VideoBuffer;
-static int g_VideoBufferLuaRef;
-
-static uint64_t g_VideoBufferStreamName = dmHashString64("rgb");
-
+struct MPEG1Video {
+    plm_t*                     m_plm;
+    plm_buffer_t*              m_StreamBuffer;
+    dmBuffer::HBuffer          m_FrameBuffer;
+    int                        m_FrameBufferLuaRef;
+};
 
 // from pl_mpeg.h with flip on the vertical axis
 static const int RI = 0;
@@ -44,71 +46,89 @@ void plm_frame_to_rgb_flip(plm_frame_t *frame, uint8_t *dest, int stride) {
             dest[d_index + 0 + GI] = plm_clamp(y - g);
             dest[d_index + 0 + BI] = plm_clamp(y + b);
             y = ((frame->y.data[y_index + 1]-16) * 76309) >> 16;
-            dest[d_index + s_BytesPerPixel + RI] = plm_clamp(y + r);
-            dest[d_index + s_BytesPerPixel + GI] = plm_clamp(y - g);
-            dest[d_index + s_BytesPerPixel + BI] = plm_clamp(y + b);
+            dest[d_index + BYTES_PER_PIXEL + RI] = plm_clamp(y + r);
+            dest[d_index + BYTES_PER_PIXEL + GI] = plm_clamp(y - g);
+            dest[d_index + BYTES_PER_PIXEL + BI] = plm_clamp(y + b);
             y = ((frame->y.data[y_index + yw]-16) * 76309) >> 16;
             dest[d_index + stride + RI] = plm_clamp(y + r);
             dest[d_index + stride + GI] = plm_clamp(y - g);
             dest[d_index + stride + BI] = plm_clamp(y + b);
             y = ((frame->y.data[y_index + yw + 1]-16) * 76309) >> 16;
-            dest[d_index + stride + s_BytesPerPixel + RI] = plm_clamp(y + r);
-            dest[d_index + stride + s_BytesPerPixel + GI] = plm_clamp(y - g);
-            dest[d_index + stride + s_BytesPerPixel + BI] = plm_clamp(y + b);
+            dest[d_index + stride + BYTES_PER_PIXEL + RI] = plm_clamp(y + r);
+            dest[d_index + stride + BYTES_PER_PIXEL + GI] = plm_clamp(y - g);
+            dest[d_index + stride + BYTES_PER_PIXEL + BI] = plm_clamp(y + b);
             c_index += 1;
             y_index += 2;
-            d_index += 2 * s_BytesPerPixel;
+            d_index += 2 * BYTES_PER_PIXEL;
         }
     }
 }
 
 // decoder video callback
-static void app_on_video(plm_t *mpeg, plm_frame_t *frame, void *user) {
-    int w = plm_get_width(g_plm);
+static void OnVideoFrameDecoded(plm_t *mpeg, plm_frame_t *frame, void *user)
+{
+    MPEG1Video* video = (MPEG1Video*)user;
+
+    // create frame buffer if needed
+    if (video->m_FrameBuffer == 0)
+    {
+        // create frame buffer to which video frames are decoded
+        int width = plm_get_width(video->m_plm);
+        int height = plm_get_height(video->m_plm);
+        const uint32_t size = width * height;
+        dmBuffer::StreamDeclaration streams_decl[] = {
+            { FRAME_BUFFER_STREAM_NAME, dmBuffer::VALUE_TYPE_UINT8, 3 }
+        };
+        dmBuffer::Create(size, streams_decl, 1, &video->m_FrameBuffer);
+
+        // reset frame buffer to 0
+        uint8_t* framedata = 0;
+        uint32_t framedatasize = 0;
+        dmBuffer::GetBytes(video->m_FrameBuffer, (void**)&framedata, &framedatasize);
+        memset(framedata, 0, framedatasize);
+    }
+
+    // get frame buffer bytes
     uint8_t* data = 0;
     uint32_t datasize = 0;
-    dmBuffer::GetBytes(g_VideoBuffer, (void**)&data, &datasize);
+    dmBuffer::GetBytes(video->m_FrameBuffer, (void**)&data, &datasize);
+
+    // decode frame to buffer
+    int w = plm_get_width(video->m_plm);
     plm_frame_to_rgb_flip(frame, data, w * 3);
-    dmBuffer::ValidateBuffer(g_VideoBuffer);
+
+    // validate the integrity of the frame buffer
+    dmBuffer::ValidateBuffer(video->m_FrameBuffer);
 }
 
-static void app_on_audio(plm_t *mpeg, plm_samples_t *samples, void *user) {
+
+static void OnAudioSamplesDecoded(plm_t *mpeg, plm_samples_t *samples, void *user)
+{
+    // MPEG1Video* video = (MPEG1Video*)user;
     // int size = sizeof(float) * samples->count * 2;
     // SDL_QueueAudio(self->audio_device, samples->interleaved, size);
 }
 
-static int Open(lua_State* L)
+
+static MPEG1Video* CheckVideo(lua_State* L, int index)
 {
-    DM_LUA_STACK_CHECK(L, 2);
-
-    // Get video buffer and create decoder
-    dmScript::LuaHBuffer* videobuffer = dmScript::CheckBuffer(L, 1);
-    uint8_t* videodata = 0;
-    uint32_t videodatasize = 0;
-    dmBuffer::GetBytes(videobuffer->m_Buffer, (void**)&videodata, &videodatasize);
-
-    // Create the decoder with the data from the video buffer
-    g_plm = plm_create_with_memory(videodata, videodatasize, false);
-    // validate opened video buffer
-    if (!plm_probe(g_plm, 5000 * 1024)) {
-        lua_pushboolean(L, 0);
-        lua_pushstring(L, "No MPEG video or audio streams found");
-        return 2;
+    MPEG1Video* video = (MPEG1Video*)(uintptr_t)luaL_checknumber(L, index);
+    if (video == 0x0)
+    {
+        luaL_error(L, "%s", "Video is null");
     }
+    return video;
+}
 
-    dmLogInfo(
-        "Opened video with framerate: %f, samplerate: %d, duration: %f",
-        plm_get_framerate(g_plm),
-        plm_get_samplerate(g_plm),
-        plm_get_duration(g_plm)
-    );
 
+static void SetVideoOptions(MPEG1Video* video, lua_State* L, int index)
+{
     // Read options and set on decoder
     bool loop_video = false;
     bool enable_audio = false;
-    if (!lua_isnil(L, 2)) {
-        luaL_checktype(L, 2, LUA_TTABLE);
-        lua_pushvalue(L, 2);
+    if (!lua_isnil(L, index)) {
+        luaL_checktype(L, index, LUA_TTABLE);
+        lua_pushvalue(L, index);
         lua_pushnil(L);
         while (lua_next(L, -2)) {
             const char* option = lua_tostring(L, -2);
@@ -124,46 +144,139 @@ static int Open(lua_State* L)
         }
         lua_pop(L, 1);
     }
-    plm_set_loop(g_plm, loop_video);
-    plm_set_audio_enabled(g_plm, enable_audio);
-    plm_set_video_decode_callback(g_plm, app_on_video, 0x0);
+    plm_set_loop(video->m_plm, loop_video);
+    plm_set_audio_enabled(video->m_plm, enable_audio);
+    plm_set_video_decode_callback(video->m_plm, OnVideoFrameDecoded, video);
     if (enable_audio) {
-        plm_set_audio_decode_callback(g_plm, app_on_audio, 0x0);
+        plm_set_audio_decode_callback(video->m_plm, OnAudioSamplesDecoded, video);
+    }
+}
+
+
+static int Open(lua_State* L)
+{
+    DM_LUA_STACK_CHECK(L, 2);
+
+    MPEG1Video* video = new MPEG1Video;
+    memset(video, 0, sizeof(*video));
+
+    uint8_t* videodata = 0;
+    size_t videodatasize = 0;
+    if (lua_isstring(L, 1))
+    {
+        videodata = (uint8_t*)luaL_checklstring(L, 1, &videodatasize);
+    }
+    else if (dmScript::IsBuffer(L, 1))
+    {
+        dmScript::LuaHBuffer* videobuffer = dmScript::CheckBuffer(L, 1);
+        uint32_t videodatacomponents = 0;
+        uint32_t videodatastride = 0;
+        dmBuffer::Result r = dmBuffer::GetStream(videobuffer->m_Buffer, dmHashString64("data"), (void**)&videodata, (uint32_t*)&videodatasize, &videodatacomponents, &videodatastride);
+        // dmBuffer::GetBytes(videobuffer->m_Buffer, (void**)&videodata, &videodatasize);
+    }
+    else
+    {
+        lua_pushboolean(L, 0);
+        luaL_error(L, "%s", "Video is null");
+        return 2;
     }
 
-    // create frame buffer to which video frames are decoded
-    int width = plm_get_width(g_plm);
-    int height = plm_get_height(g_plm);
-    const uint32_t size = width * height;
-    dmBuffer::StreamDeclaration streams_decl[] = {
-        {g_VideoBufferStreamName, dmBuffer::VALUE_TYPE_UINT8, 3}
-    };
-    dmBuffer::Create(size, streams_decl, 1, &g_VideoBuffer);
+    // Create the decoder with the data from the video buffer
+    bool free_when_done = false;
+    video->m_plm = plm_create_with_memory(videodata, videodatasize, free_when_done);
+    // validate opened video buffer
+    if (!plm_probe(video->m_plm, 5000 * 1024)) {
+        lua_pushboolean(L, 0);
+        lua_pushstring(L, "No MPEG video or audio streams found");
+        return 2;
+    }
 
-    // reset video frame buffer to 0
-    uint8_t* framedata = 0;
-    uint32_t framedatasize = 0;
-    dmBuffer::GetBytes(g_VideoBuffer, (void**)&framedata, &framedatasize);
-    memset(framedata, 0, framedatasize);
-
-    // increase ref count of video frame buffer to avoid GC
-    dmScript::LuaHBuffer buffer(g_VideoBuffer, dmScript::OWNER_C);
-    dmScript::PushBuffer(L, buffer);
-    g_VideoBufferLuaRef = dmScript::Ref(L, LUA_REGISTRYINDEX);
+    SetVideoOptions(video, L, 2);
 
     // ok!
-    lua_pushboolean(L, 1);
+    lua_pushnumber(L, (uintptr_t)video);
     lua_pushnil(L);
     return 2;
-
 }
+
+
+static int Stream(lua_State* L)
+{
+    DM_LUA_STACK_CHECK(L, 2);
+
+    MPEG1Video* video = new MPEG1Video;
+    memset(video, 0, sizeof(*video));
+
+    video->m_StreamBuffer = plm_buffer_create_with_capacity(STREAMING_BUFFER_CAPACITY);
+
+    int destroy_when_done = true;
+    video->m_plm = plm_create_with_buffer(video->m_StreamBuffer, destroy_when_done);
+    // plm_buffer_set_load_callback(video->m_StreamBuffer, OnVideoBufferNeedsData, video);
+
+    SetVideoOptions(video, L, 1);
+
+    // ok!
+    lua_pushnumber(L, (uintptr_t)video);
+    lua_pushnil(L);
+    return 2;
+}
+
+
+static int Write(lua_State* L)
+{
+    DM_LUA_STACK_CHECK(L, 2);
+
+    MPEG1Video* video = CheckVideo(L, 1);
+
+    if (video->m_StreamBuffer == 0)
+    {
+        lua_pushnil(L);
+        lua_pushstring(L, "Video was not created using stream()");
+        return 0;
+    }
+
+    size_t length = 0;
+    uint8_t* bytes = (uint8_t*)luaL_checklstring(L, 2, &length);
+
+    if (length == 0)
+    {
+        plm_buffer_signal_end(video->m_StreamBuffer);
+        lua_pushnumber(L, 0);
+        lua_pushnil(L);
+    }
+    else
+    {
+        size_t bytes_written = plm_buffer_write(video->m_StreamBuffer, bytes, length);
+        lua_pushnumber(L, bytes_written);
+        lua_pushnil(L);
+    }
+    return 0;
+}
+
 
 static int Close(lua_State* L)
 {
     DM_LUA_STACK_CHECK(L, 0);
-    dmScript::Unref(L, LUA_REGISTRYINDEX, g_VideoBufferLuaRef); // We want it destroyed by the GC
-    plm_destroy(g_plm);
-    g_plm = 0;
+
+    MPEG1Video* video = CheckVideo(L, 1);
+
+    if (video->m_FrameBufferLuaRef != 0)
+    {
+        // We want it destroyed by the GC
+        dmScript::Unref(L, LUA_REGISTRYINDEX, video->m_FrameBufferLuaRef);
+        video->m_FrameBufferLuaRef = 0;
+    }
+
+    if (video->m_FrameBuffer != 0)
+    {
+        dmBuffer::Destroy(video->m_FrameBuffer);
+        video->m_FrameBuffer = 0;
+    }
+
+    plm_destroy(video->m_plm);
+    video->m_plm = 0;
+
+    delete video;
     return 0;
 }
 
@@ -171,10 +284,11 @@ static int Close(lua_State* L)
 static int Decode(lua_State* L)
 {
     DM_LUA_STACK_CHECK(L, 0);
-    double seconds = luaL_checknumber(L, 1);
-    if (!plm_has_ended(g_plm))
+    MPEG1Video* video = CheckVideo(L, 1);
+    double seconds = luaL_checknumber(L, 2);
+    if (!plm_has_ended(video->m_plm))
     {
-        plm_decode(g_plm, seconds);
+        plm_decode(video->m_plm, seconds);
     }
     return 0;
 }
@@ -183,40 +297,84 @@ static int Decode(lua_State* L)
 static int Seek(lua_State* L)
 {
     DM_LUA_STACK_CHECK(L, 0);
-    double time = luaL_checknumber(L, 1);
-    bool seek_exact = lua_toboolean(L, 2);
-    plm_seek(g_plm, time, seek_exact);
+    MPEG1Video* video = CheckVideo(L, 1);
+    double time = luaL_checknumber(L, 2);
+    bool seek_exact = lua_toboolean(L, 3);
+    plm_seek(video->m_plm, time, seek_exact);
     return 0;
 }
+
+
+static int Rewind(lua_State* L)
+{
+    DM_LUA_STACK_CHECK(L, 0);
+    MPEG1Video* video = CheckVideo(L, 1);
+    plm_rewind(video->m_plm);
+    return 0;
+}
+
 
 static int GetInfo(lua_State* L)
 {
     DM_LUA_STACK_CHECK(L, 1);
 
+    MPEG1Video* video = CheckVideo(L, 1);
+
     lua_newtable(L);
     lua_pushstring(L, "width");
-    lua_pushnumber(L, plm_get_width(g_plm));
+    lua_pushnumber(L, plm_get_width(video->m_plm));
     lua_rawset(L, -3);
 
     lua_pushstring(L, "height");
-    lua_pushnumber(L, plm_get_height(g_plm));
+    lua_pushnumber(L, plm_get_height(video->m_plm));
+    lua_rawset(L, -3);
+
+    lua_pushstring(L, "framerate");
+    lua_pushnumber(L, plm_get_framerate(video->m_plm));
+    lua_rawset(L, -3);
+
+    lua_pushstring(L, "samplerate");
+    lua_pushnumber(L, plm_get_samplerate(video->m_plm));
     lua_rawset(L, -3);
 
     lua_pushstring(L, "bytes_per_pixel");
-    lua_pushnumber(L, s_BytesPerPixel);
+    lua_pushnumber(L, BYTES_PER_PIXEL);
     lua_rawset(L, -3);
 
     lua_pushstring(L, "time");
-    lua_pushnumber(L, plm_get_time(g_plm));
+    lua_pushnumber(L, plm_get_time(video->m_plm));
     lua_rawset(L, -3);
 
     lua_pushstring(L, "duration");
-    lua_pushnumber(L, plm_get_duration(g_plm));
+    lua_pushnumber(L, plm_get_duration(video->m_plm));
     lua_rawset(L, -3);
 
     lua_pushstring(L, "eos");
-    lua_pushboolean(L, plm_has_ended(g_plm));
+    lua_pushboolean(L, plm_has_ended(video->m_plm));
     lua_rawset(L, -3);
+
+    lua_pushstring(L, "is_looping");
+    lua_pushboolean(L, plm_get_loop(video->m_plm));
+    lua_rawset(L, -3);
+
+    lua_pushstring(L, "is_streaming");
+    lua_pushboolean(L, (video->m_StreamBuffer != 0));
+    lua_rawset(L, -3);
+
+    if (video->m_StreamBuffer)
+    {
+        lua_pushstring(L, "streaming_buffer_bytes_remaining");
+        lua_pushnumber(L, plm_buffer_get_remaining(video->m_StreamBuffer));
+        lua_rawset(L, -3);
+
+        lua_pushstring(L, "streaming_buffer_capacity");
+        lua_pushnumber(L, video->m_StreamBuffer->capacity);
+        lua_rawset(L, -3);
+
+        lua_pushstring(L, "streaming_buffer_size");
+        lua_pushnumber(L, plm_buffer_get_size(video->m_StreamBuffer));
+        lua_rawset(L, -3);
+    }
 
     return 1;
 }
@@ -226,7 +384,23 @@ static int GetFrame(lua_State* L)
 {
     DM_LUA_STACK_CHECK(L, 1);
 
-    lua_rawgeti(L,LUA_REGISTRYINDEX, g_VideoBufferLuaRef);
+    MPEG1Video* video = CheckVideo(L, 1);
+
+    if (video->m_FrameBuffer != 0)
+    {
+        if (video->m_FrameBufferLuaRef == 0)
+        {
+            // increase ref count of frame buffer to avoid GC
+            dmScript::LuaHBuffer buffer(video->m_FrameBuffer, dmScript::OWNER_C);
+            dmScript::PushBuffer(L, buffer);
+            video->m_FrameBufferLuaRef = dmScript::Ref(L, LUA_REGISTRYINDEX);
+        }
+        lua_rawgeti(L, LUA_REGISTRYINDEX, video->m_FrameBufferLuaRef);
+    }
+    else
+    {
+        lua_pushnil(L);
+    }
 
     return 1;
 }
@@ -234,8 +408,11 @@ static int GetFrame(lua_State* L)
 static const luaL_reg Module_methods[] =
 {
     {"open", Open},
+    {"stream", Stream},
+    {"write", Write},
     {"close", Close},
     {"decode", Decode},
+    {"rewind", Rewind},
     {"seek", Seek},
     {"get_info", GetInfo},
     {"get_frame", GetFrame},
